e55ccac8544f4d35cebb97c42a6d861afbf29f29
learnICT <kibrisli7@gmail.com>
2015-02-18 23:19:13 +0000

    Update file_read.py
    added some commentary to better remember some of the code.

--- sandBox/Files/file_read.py
+++ sandBox/Files/file_read.py
@@ -2,9 +2,12 @@ from sys import argv
 
 script, filename = argv
 
+# The variable txt runs a command to open the filename passed through the parameters (In this case called filename)
 txt = open(filename)
 
+#prints the line, and concatenates the filename.
 print "Here's your file %r:" % filename
+#prints contents of the file
 print txt.read()
 
 print "Type the filename again:"
